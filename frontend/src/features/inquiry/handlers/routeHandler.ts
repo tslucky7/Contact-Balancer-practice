c6c52f1a63a18a2cbb0dc04abd45108bf500386b
TS <takumi1222122212221222@gmail.com>
2026-03-09 14:23:22 +0000

    feat: 確認画面のリロード時の項目入力の保存・その他リファクタリング
    - 値の保存はsessionStorageで保存

--- frontend/src/features/inquiry/handlers/routeHandler.ts
+++ frontend/src/features/inquiry/handlers/routeHandler.ts
@@ -1,5 +1,8 @@
 import { dom, state } from "../state/context";
 import { STEPS } from "../types/types";
+import { resetStepsToEdit } from "../dom/utils";
+import { toConfirmHandler } from "./toConfirmHandler";
+import { loadSession, writeForm } from "../adapters/formDataAdapter";
 
 /**
  * ルートを処理する
@@ -10,41 +13,60 @@ export const routeHandler = (): void => {
 
   switch (pathname) {
     case STEPS.EDIT:
-      // URLが入力画面なら、入力画面を表示する処理を呼ぶ
       console.log('case: STEPS.EDIT');
-      dom.stepEdit.classList.remove('hidden');
-      dom.stepConfirm.replaceChildren();
-      dom.stepComplete.replaceChildren();
+      restoreFormFromSession(dom.form);
+      resetStepsToEdit(dom);
 
       break;
     case STEPS.CONFIRM:
-      // 入力データがない場合は入力画面へ戻すなどの制御もここで行う
       console.log('case: STEPS.CONFIRM');
+      restoreFormFromSession(dom.form);
+      setStateFromSession();
+
+      // 入力データがない場合は入力画面へ戻すなどの制御もここで行う
       if (!state.name || !state.email || !state.subject || !state.message) {
         history.replaceState(null, '', STEPS.EDIT);
-        return;
+        break;
       }
 
-      // URLが確認画面なら、確認画面を表示する処理を呼ぶ
-      dom.stepEdit.classList.add('hidden');
-      dom.stepComplete.replaceChildren();
+      toConfirmHandler(new Event('click'));
       
       break;
     case STEPS.COMPLETE:
-      // URLが完了画面なら、完了画面を表示する
+      // URLが完了画面で読み込まれた場合は自動的に入力画面に戻す
       console.log('case: STEPS.COMPLETE');
-      dom.stepEdit.classList.remove('hidden');
-      dom.stepConfirm.replaceChildren();
       history.replaceState(null, '', STEPS.EDIT);
 
       break;
     default:
-      // それ以外（空や #edit）なら、入力画面を表示する
+      // それ以外（空や #edit）なら、入力画面を表示する（デフォルトは入力画面）
       console.log('case: default');
-      dom.stepEdit.classList.remove('hidden');
-      dom.stepConfirm.replaceChildren();
-      dom.stepComplete.replaceChildren();
+      resetStepsToEdit(dom);
 
       break;
   }
+};
+
+/**
+ * sessionStorage からフォームの各入力欄に値を復元する
+ */
+const restoreFormFromSession = (form: HTMLFormElement): void => {
+  const savedValues = loadSession();
+  if (!savedValues) return;
+
+  // フォームの各要素に値をセットする
+  writeForm(form, savedValues);
+};
+
+/**
+ * sessionStorage から状態を復元する
+ */
+const setStateFromSession = (): void => {
+  const savedValues = loadSession();
+  if (!savedValues) return;
+
+  state.name = savedValues?.name || '';
+  state.email = savedValues?.email || '';
+  state.subject = savedValues?.subject || '';
+  state.message = savedValues?.message || '';
 };
